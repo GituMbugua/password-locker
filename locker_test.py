837dff5a7bc980af762b78c63239df83fee15852
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-13 16:24:15 +0300

    add test for initializing class

--- locker_test.py
+++ locker_test.py
@@ -8,6 +8,13 @@ class TestLocker(unittest.TestCase):
         set-up method
         '''
         self.new_cred = Credential("gitu", "1234asdf") # password object
+
+    def test_init(self):
+        '''
+        test to see if the object is initializing
+        '''
+        self.assertEqual(self.new_cred.user_name, "gitu")
+        self.assertEqual(self.new_cred.password, "1234asdf")
                 
 
 if __name__ == '__main__':
