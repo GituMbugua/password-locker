f469f8a836ce7a77f2ed911c77c48ebb603380a7
Gitu Mbugua <gmbugua38@gmail.com>
2017-10-15 19:59:15 +0300

    remove one item from class

--- locker.py
+++ locker.py
@@ -6,7 +6,7 @@ class Credential:
     '''
     password_list = []
     
-    def __init__(self, first_name, user_name, password):
+    def __init__(self, user_name, password):
         self.user_name = user_name
         self.password = password
         
@@ -30,6 +30,13 @@ class Credential:
         for credential in cls.password_list:
             if credential.user_name == name:
                 return credential
+            
+    @classmethod
+    def credential_exist(cls, name):
+        for credential in cls.password_list:
+            if credential.user_name == name:
+                return True
+        return False
     
     @classmethod
     def display_passwords(cls):
